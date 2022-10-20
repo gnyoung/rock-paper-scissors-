9da105b82af6a241026b8e464d6095748041ec70
Gabby Young <gabrielleyoung10@gmail.com>
2022-10-20 14:53:09 -0500

    Add basic UI to web page

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,65 +1,88 @@
+//Create find elements with ids and store in variables to use with DOM methods
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const results = document.querySelector('#results');
 //define variable for array 
-let array = ["rock", "paper", "scissors"];
-
+const rpsArray = ["rock", "paper", "scissors"];
+// Set scores to zero
+let playerScore = 0;
+let computerScore = 0;
 // create a function for the computer to randomly make a choice and store value in variable "computerSelection"
 function getComputerChoice(array) {
     return array[Math.floor(Math.random()*array.length)];
 }
-computerSelection = getComputerChoice(array);
-//console.log(computerSelection);
-
-//prompt user to input value and convert value to lower case 
-let playerSelection = prompt("Choose rock, paper, or scissors:");
-if (playerSelection != null) {
- playerSelection.toLowerCase();
-}
-//console.log(playerSelection);
+let computerSelection = getComputerChoice(rpsArray); 
 
 //create a function that plays a single round of the game by comparing user and computer choices
-function playRound(playerSelectionParameter, computerSelectionParameter) {
-    if ((playerSelectionParameter == "rock") && (computerSelectionParameter == "scissors")) {
-        return "You win! Rock beats scissors."; 
-    } else if ((playerSelectionParameter == "scissors") && (computerSelectionParameter == "rock")) {
-        return "You lose! Rock beats scissors."; 
-    } else if ((playerSelectionParameter == "scissors") && (computerSelectionParameter == "paper")) {
-        return "You win! Scissors beats paper.";
-    } else if ((playerSelectionParameter == "paper") && (computerSelectionParameter == "scissors")) {
-        return "You lose! Scissors beats paper.";
-    } else if ((playerSelectionParameter == "paper") && (computerSelectionParameter == "rock")) {
-        return "You win! Paper beats rock.";
-    } else if ((playerSelectionParameter == "rock") && (computerSelectionParameter == "paper")) {
-        return "You lose! Paper beats rock.";
-    } else if (playerSelectionParameter == null) {
-        return "Please enter a value.";
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+        playerScore++;
+        const p = document.createElement('p');
+        p.innerText = "You win! Rock beats scissors."; 
+        results.appendChild(p);
+    } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+        computerScore++;
+        const p = document.createElement('p');
+        p.innerText = "You lose! Rock beats scissors.";
+        results.appendChild(p);
+    } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+        playerScore++;
+        const p = document.createElement('p');
+        p.innerText = "You win! Scissors beats paper.";
+        results.appendChild(p);
+    } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+        computerScore++;
+        const p = document.createElement('p');
+        p.innerText = "You lose! Scissors beats paper.";
+        results.appendChild(p);
+    } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+        playerScore++;
+        const p = document.createElement('p');
+        p.innerText = "You win! Paper beats rock.";
+        results.appendChild(p);
+    } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+        computerScore++;
+        const p = document.createElement('p');
+        p.innerText = "You lose! Paper beats rock.";
+        results.appendChild(p);
+    } else if (playerSelection === computerSelection) {
+        const p = document.createElement('p');
+        p.innerText = "It's a tie!";
+        results.appendChild(p);
     } else {
-        return "Enter correct value."}
-    } 
+        const p = document.createElement('p');
+        p.innerText = "Enter correct value.";
+        results.appendChild(p);
+    }
+} 
 
-//add line to function for an input that is not rock, paper or scissors 
+//Create an eventListner that runs the playRound function when a user clicks a button 
+rock.addEventListener('click', () => {
+    playRound("rock", computerSelection);
+    checkForWinner(playerScore, computerScore);
+});
+paper.addEventListener('click', () => {
+    playRound("paper", computerSelection);
+    checkForWinner(playerScore, computerScore);
+});
+scissors.addEventListener('click', () => {
+    playRound("scissors", computerSelection);
+    checkForWinner(playerScore, computerScore);
+});
 
-//console.log(playRound(playerSelection, computerSelection));
 
-//Create function that loops through five rounds
- function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult == "You win! Rock beats scissors." || roundResult == "You win! Scissors beats paper." || roundResult == "You win! Paper beats rock.") {
-            ++playerScore;
-        } else if (roundResult == "You lose! Rock beats scissors." || roundResult == "You lose! Scissors beats paper." || roundResult == "You lose! Paper beats rock."){
-            ++computerScore;
-        } }
+function checkForWinner(playerScore, computerScore) {
+  if (playerScore == 5) {
+    const p = document.createElement('p');
+    p.innerHTML = "<strong>Congrats, you win the game!</strong>";
+    results.appendChild(p);
     
-    if (playerScore > computerScore) {
-        return "You win!";
-    } else if (playerScore < computerScore) {
-        return "You lose."; 
-    } else { 
-        return "It's a tie!";
-    } 
+  }
+  else if (computerScore == 5) {
+    const p = document.createElement('p');
+    p.innerHTML = "<strong>Sorry, you lose the game!</strong>";
+    results.appendChild(p);
+  }
 }
-
-console.log(game()); 
-
 
